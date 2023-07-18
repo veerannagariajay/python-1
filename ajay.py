99c39c39a828a1d84f47ff042566f09b1449acbf
ajay <veerannagariajay@gmail.com>
2023-07-18 18:57:04 +0530

    version 1

---
+++ ajay.py
@@ -0,0 +1,9 @@
+class Myclass:
+    a=5
+    def hello(self):
+        print('hello world')
+        myc=Myclass()
+        print(myc.a)
+        print(myc.hello())
+   
+
---
+++ s.py
@@ -0,0 +1,4 @@
+from math import factorial as f
+
+factorial_10 = f(10)
+print(factorial_10)
